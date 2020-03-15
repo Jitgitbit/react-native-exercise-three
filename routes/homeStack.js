3085e5deb4e65a26dc86f45ec4a3b4fa37f44471
Jitgitbit <rowowolf@gmail.com>
2020-03-15 14:15:28 +0100

    screens configured

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,5 +1,14 @@
 import { createStackNavigator } from "react-navigation-stack";
+import Home from "../screens/Home";
+import ReviewDetails from "../screens/ReviewDetails";
 
-const screens = {}
+const screens = {
+  Home:{
+    screen: Home
+  },
+  ReviewDetails:{
+    screen: ReviewDetails
+  }
+}
 
 const homeStack = createStackNavigator(screens);
