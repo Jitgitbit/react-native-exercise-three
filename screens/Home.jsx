e3597d13e5eb76857598a34dfac6665e6842348b
Jitgitbit <rowowolf@gmail.com>
2020-03-15 16:34:28 +0100

    prep to pass data

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 import { globalStyles } from '../styles/global'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function Home({navigation}) {
-  const pressHandler = () => {
-    navigation.navigate('ReviewDetails'); // does the same as .push !!
-    // navigation.push('ReviewDetails');
-  }
+  const [review, setReview] = useState([
+    {title:'Maverick', rating:5, body:'not bad', key:'1'},
+    {title:'Good Midnight', rating:6, body:'not too bad', key:'2'},
+    {title:'Lord of the Rings', rating:7, body:'too long', key:'3'}
+  ])
+  // const pressHandler = () => {
+  //   navigation.navigate('ReviewDetails'); // does the same as .push !!
+  //   // navigation.push('ReviewDetails');
+  // }
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title='go to review details' onPress={pressHandler}/>
+      {/* <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title='go to review details' onPress={pressHandler}/> */}
+      <FlatList
+      data={reviews}
+      renderItem={({item}) => (
+        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+          <Text style={globalStyles.titleText}>{item.title}</Text>
+        </TouchableOpacity>
+      )}/>
     </View>
   )
 }
