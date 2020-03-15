c45ee0894eb3301b84cd6023a4e884f7de082aaa
Jitgitbit <rowowolf@gmail.com>
2020-03-15 16:39:09 +0100

    navigation.getParam !

--- screens/ReviewDetails.jsx
+++ screens/ReviewDetails.jsx
@@ -3,14 +3,17 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 import { globalStyles } from '../styles/global'
 
 export default function ReviewDetails({navigation}) {
-  const pressHandler = () => {
-    navigation.goBack(); // does the same as .push !!
-    // navigation.push('ReviewDetails');
-  }
+  // const pressHandler = () => {
+  //   navigation.goBack(); // does the same as .push !!
+  //   // navigation.push('ReviewDetails');
+  // }
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
-      <Button title='go to overview' onPress={pressHandler}/>
+      {/* <Text>ReviewDetails Screen</Text>
+      <Button title='go to overview' onPress={pressHandler}/> */}
+      <Text>title: {navigation.getParam('title')}</Text>
+      <Text>review: {navigation.getParam('body')}</Text>
+      <Text>rating: {navigation.getParam('rating')}</Text>
     </View>
   )
 }
