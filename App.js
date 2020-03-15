f47215645412e252a5fd03c88bbdc3680325ed0e
Jitgitbit <rowowolf@gmail.com>
2020-03-15 12:46:47 +0100

    importing and getting fonts

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import Home from './screens/Home';
+import * as Font from 'expo-font';
 
 export default function App() {
+
+  const getFonts = () => {
+    return Font.loadAsync({
+      'nunito-regular' : require('./assets/fonts/Nunito/Nunito-Regular.ttf'),
+      'nunito-bold' : require('./assets/fonts/Nunito/Nunito-Bold.ttf')
+    })
+  }
+
   return (
     <Home/>
   );
