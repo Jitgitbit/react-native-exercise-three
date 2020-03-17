0f6798b0288715e78afabec90d2749b28f3141f6
Jitgitbit <rowowolf@gmail.com>
2020-03-17 19:05:13 +0100

    passing a renderfn with formikProps in Formik !!!

--- components/screens/ReviewForm.jsx
+++ components/screens/ReviewForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { globalStyles } from '../../styles/global'
 import { View, Text, StyleSheet, Button, ImageBackground, Modal } from 'react-native'
 import {Formik} from 'formik'
+import { TextInput } from 'react-native-gesture-handler'
 
 
 export default function ReviewForm() {
@@ -9,8 +10,18 @@ export default function ReviewForm() {
     <View style={globalStyles.container}>
       <Formik
       initialValues={{title:'', body:'', rating:''}}
-      onSubmit={(values) => {}}>
-        
+      onSubmit={(values) => {}}
+      >
+        {(formikProps) => (
+          <View>
+            <TextInput
+            style={globalStyles.input}
+            placeholder='Review Title'
+            onChangeText={formikProps.handleChange('title')}       // =====> handles the state for us behind the scenes !!!
+            value={formikProps.values.title}                       // =====> two way data binding !!!
+            />
+          </View>
+        )}
       </Formik>
     </View>
   )
